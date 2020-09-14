c7164f08ff58b6e3f6858611d71fbdea14e3da26
jolanda <thelapegna@gmail.com>
2020-09-14 17:50:28 +0200

    finished mile stone 2

--- script.js
+++ script.js
@@ -22,7 +22,36 @@ $(".submit-button").click(function() {
 
 // How to have the comp respond
 
+    setTimeout(response, 1000);
 
+    function response(){
+
+        var msgReply = 'ok';
+
+        var compBlankMessage = $('#template .msgsent').clone().addClass('msgrecieved-style');
+
+        compBlankMessage.find('.testo').text(msgReply + " " + time)
+
+        $('.message-screen').append(compBlankMessage);
+    }
 
     })
+});
+
+// Searching contacts in the search bar
+
+$('#contacts-filter').keyup(function(event){
+
+    var userSearch = $(this).val().toLowerCase();
+
+    $('.contact').each(function(){
+        var contactNames = $(this).find('.name').text().toLowerCase();
+  
+        if (contactNames.includes(userSearch)){
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      })
+
 });
