14c5419a7c0abb28c522749750dc891d66efdf8b
jolanda <thelapegna@gmail.com>
2020-09-14 13:45:55 +0200

    finished first milestone

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
 
+// How to add user input to the MSG screen
 
+$(".submit-button").click(function() {
+    // When the submit button is clicked a function is called 
+
+    var userInput = $('.message').val();
+
+    var blankMessage = $('#template .msgsent').clone().addClass('msgsent-style');
+
+    // Current Time
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
+
+    blankMessage.find(".testo").text(userInput + " " + time);
+
+    $('.message-screen').append(blankMessage);
+
+    $(".message").val("");
+
+
+// How to have the comp respond
+
+
+
+    })
 });
